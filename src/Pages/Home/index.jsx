4fc4e2976f7c983f8ebe64e25585806eb50f537d
k9opAA <engr.aliazgor@gmail.com>
2026-01-15 22:24:02 +0600

    zabir bosailam

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,7 +2,7 @@ const Home=()=>{
     return(
         <div className="home-page">
             <div className="container py-8">
-                <h1 className="text-4xl font-bold mb-4">Welcome to E-mart</h1>
+                <h1 className="text-4xl font-bold mb-4">Welcome to Zabir</h1>
                 <p className="text-lg">Your one-stop shop for all your needs. Quack Quack Duckling</p>
             </div>
         </div>
