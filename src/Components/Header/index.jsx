e31c061d9cf28bd767d110dbe6881ce38e5ab749
k9opAA <engr.aliazgor@gmail.com>
2026-01-15 22:16:53 +0600

    saving work before switch

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -3,7 +3,7 @@ const Header=()=>{
         <div className="HeaderWrapper">
             <div className="top-stripe bg-purple">
                 <div className="container">
-                    <p className="mb-0 text-center">Welcome to <b>E-mart!</b> Your one-stop shop for all your needs.
+                    <p className="mb-0 p-0 text-center">Welcome to <b>E-mart!</b> Your one-stop shop for all your needs.
                     </p>
                 </div>
             </div>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,8 +2,7 @@ const Home=()=>{
     return(
         <div className="home-page">
             <div className="container py-8">
-                <h1 className="text-4xl font-bold mb-4">Welcome to E-mart</h1>
-                <p className="text-lg">Your one-stop shop for all your needs.</p>
+                <h1 className="text-3xl text-center font-bold mb-4">Welcome to E-mart!</h1>
             </div>
         </div>
     )
